8084f6cf750b9bb423175891fee3edccbbb101fd
database management <53146593+nguyenvantien1999@users.noreply.github.com>
2019-12-23 20:26:20 +1400

    Update management_backEnd.py

--- management_backEnd.py
+++ management_backEnd.py
@@ -1,5 +1,100 @@
 import sqlite3
 #backEnd
+#======================================================Hóa đơn================================================================================
+def HoaDonResult():
+    con = sqlite3.connect("HoaDonRecord.db")
+    cur = con.cursor()
+    cur.execute('CREATE TABLE IF NOT EXISTS HoaDonRecord(id INTEGER PRIMARY KEY , \
+        HoaDonID text, NgayBan text, TongTriGia money)')
+    con.commit()
+    con.close()
+
+def addHoaDonNewRec(HoaDonID, NgayBan, TongTriGia):
+    con = sqlite3.connect("HoaDonRecord.db")
+    cur = con.cursor()
+    cur.execute('INSERT INTO HoaDonRecord VALUES (NULL, ?,?,?)',(HoaDonID, NgayBan, TongTriGia))
+    con.commit()
+    con.close()
+
+def viewHoaDonData():
+    con = sqlite3.connect("HoaDonRecord.db")
+    cur = con.cursor()
+    cur.execute('SELECT * FROM HoaDonRecord')
+    rows = cur.fetchall()
+    con.close()
+    return rows
+
+def deleteHoaDonDataRec(id):
+    con = sqlite3.connect("HoaDonRecord.db")
+    cur = con.cursor()
+    cur.execute('DELETE FROM HoaDonRecord WHERE id=?', (id,))
+    con.commit()
+    con.close()
+
+def searchHoaDonDataRec(HoaDonID='', NgayBan='', TongTriGia=''):
+    con = sqlite3.connect("HoaDonRecord.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM HoaDonRecord WHERE HoaDonID=? OR NgayBan=? OR TongTriGia=?",\
+        (HoaDonID, NgayBan, TongTriGia))
+    rows = cur.fetchall() 
+    con.close()
+    return rows
+#======================================================Chi tiết hóa đơn=======================================================================
+def CTHDResult():
+    con = sqlite3.connect("HoaDonRecord.db")
+    cur = con.cursor()
+    cur.execute('CREATE TABLE IF NOT EXISTS CTHDRecord(id INTEGER PRIMARY KEY , \
+        HoaDonID text, hangMuaID text, tenHangMua text, SLMua int, donGiaBanCTHD int)')
+    con.commit()
+    con.close()
+
+def addCTHDNewRec(HoaDonID, hangMuaID, tenHangMua, SLMua, donGiaBanCTHD):
+    con = sqlite3.connect("HoaDonRecord.db")
+    cur = con.cursor()
+    cur.execute('INSERT INTO CTHDRecord VALUES (NULL, ?,?,?,?,?)',(HoaDonID, hangMuaID, tenHangMua, SLMua, donGiaBanCTHD))
+    con.commit()
+    con.close()
+
+def viewCTHDData():
+    con = sqlite3.connect("HoaDonRecord.db")
+    cur = con.cursor()
+    cur.execute('SELECT * FROM CTHDRecord')
+    rows = cur.fetchall()
+    con.close()
+    return rows
+
+def deleteCTHDDataRec(id):
+    con = sqlite3.connect("HoaDonRecord.db")
+    cur = con.cursor()
+    cur.execute('DELETE FROM CTHDRecord WHERE id=?', (id,))
+    con.commit()
+    con.close()
+
+def searchCTHDDataRec(HoaDonID='', hangMuaID='', tenHangMua='', SLMua='', donGiaBanCTHD=''):
+    con = sqlite3.connect("HoaDonRecord.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM CTHDRecord WHERE HoaDonID=? OR hangMuaID=? OR tenHangMua=? OR SLMua=? OR donGiaBanCTHD=?",\
+        (HoaDonID,hangMuaID, tenHangMua, SLMua, donGiaBanCTHD))
+    rows = cur.fetchall() 
+    con.close()
+    return rows
+
+def TongTriGiaRec(HoaDonID=''):
+    con = sqlite3.connect("HoaDonRecord.db")
+    cur = con.cursor()
+    cur.execute('SELECT SUM(donGiaBanCTHD*SLMua) FROM CTHDRecord WHERE HoaDonID=?',(HoaDonID,))
+    rows = cur.fetchall() 
+    con.close()
+    return rows
+
+def displayCTHDRec(HoaDonID=''):
+    con = sqlite3.connect("HoaDonRecord.db")
+    cur = con.cursor()
+    cur.execute('SELECT hangMuaID, tenHangMua, SLMua, donGiaBanCTHD FROM CTHDRecord WHERE HoaDonID=?',(HoaDonID,))
+    rows = cur.fetchall() 
+    con.close()
+    return rows
+
 #======================================================Khach Hang=============================================================================
 def khachHangResult():
     con = sqlite3.connect("khachHangRecord.db")
@@ -84,4 +179,6 @@ def searchDataRec(hangID='', tenHang='', DVTinh='', SoLuong='', donGiaNhap='', d
 
 
 #hangResult()
-khachHangResult()
+#khachHangResult()
+CTHDResult()
+HoaDonResult()
